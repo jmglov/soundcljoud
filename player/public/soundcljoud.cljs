d329022fd51ee0052dfcff4043eb25ff565f583b
Josh Glover <jmglov@jmglov.net>
2024-06-09 10:30:09 +0200

    player: load and play album

--- player/public/soundcljoud.cljs
+++ player/public/soundcljoud.cljs
@@ -4,6 +4,31 @@
 (defn log [msg obj]
   (js/console.log msg obj))
 
+(defn get-el [selector]
+  (if (instance? js/HTMLElement selector)
+    selector  ; already an element; just return it
+    (js/document.querySelector selector)))
+
+(defn set-styles! [el styles]
+  (set! (.-style el) styles))
+
+(defn append-children! [id children]
+  (let [children (if (sequential? children) children [children])  ; handle single child
+        el (get-el id)]
+    (doseq [child children]
+      (.appendChild el child))
+    el))
+
+(defn remove-children! [id]
+  (let [el (get-el id)]
+    (while (.-firstChild el)
+      (.removeChild el (.-lastChild el)))
+    el))
+
+(defn set-children! [id children]
+  (-> (remove-children! id)
+      (append-children! children)))
+
 (defn fetch-xml [path]
   (log "Fetching path:" path)
   (let [parse #(.parseFromString (js/window.DOMParser.) % "text/xml")]
@@ -11,22 +36,68 @@
           (.text)
           parse)))
 
-(comment
+(defn xml-get [el k]
+  (-> el
+      (.querySelector k)
+      (.-innerHTML)))
 
-  (def base-path "/albums/Garth+Brooks/Fresh+Horses/")
-  ;; => #'soundcljoud/base-path
+(defn xml-get-attr [el k attr]
+  (-> el
+      (.querySelector k)
+      (.getAttribute attr)))
 
-  (def audio (js/document.querySelector "audio"))
+(defn ->track [item-el]
+  {:artist (xml-get item-el "author")
+   :title (xml-get item-el "title")
+   :number (-> (xml-get item-el "episode") js/parseInt)
+   :src (xml-get-attr item-el "enclosure" "url")})
 
-  (set! (.-src audio) (str base-path "Garth+Brooks+-+Ireland.mp3"))
+(defn ->album [xml]
+  {:title (xml-get xml "title")
+   :image (xml-get-attr xml "image" "href")
+   :tracks (->> (.querySelectorAll xml "item")
+                (map ->track)
+                (sort-by :number))})
 
-  (.-src audio)
-  ;; => "/albums/Garth+Brooks/Fresh+Horses/Garth+Brooks+-+Ireland.mp3"
+(defn load-album [path]
+  (p/-> (fetch-xml path) ->album))
 
-  (p/-> (str base-path "album.rss")
-        fetch-xml
-        log)
+(defn play-track! [{:keys [number src] :as track}]
+  (log "Playing track" (clj->js track))
+  (let [track-spans (seq (.-children (get-el "#tracks")))]
+    (doseq [span track-spans]
+      (set-styles! span "font-weight: normal;"))
+    (-> track-spans
+        (nth (dec number))
+        (set-styles! "font-weight: bold;")))
+  (set! (.-src (get-el "audio")) src))
 
-  (log "foo")
+(defn track->span [track]
+  (let [span (js/document.createElement "span")
+        {:keys [number title]} track]
+    (set! (.-innerHTML span) (str number ". " title))
+    (.addEventListener span "click" (partial play-track! track))
+    span))
 
-  )
+(defn display-tracks! [tracks]
+  (->> tracks
+       (map track->span)
+       (set-children! "#tracks")))
+
+(defn display-album! [{:keys [image title tracks] :as album}]
+  (let [img (get-el "img")]
+    (set! (.-src img) image)
+    (set! (.-alt img) title))
+  (let [display-title (str "Soundcljoud: " title)]
+    (set! (.-innerHTML (get-el "h1")) display-title)
+    (set! (.-title js/document) display-title))
+  (set-styles! (get-el "#player") "display: flex; gap: 3%;")
+  (display-tracks! tracks)
+  album)
+
+(defn load-ui! [base-path]
+  (p/let [album (load-album (str base-path "album.rss"))]
+    (display-album! album)
+    (play-track! (-> album :tracks first))))
+
+(load-ui! "/albums/Garth+Brooks/Fresh+Horses/")
